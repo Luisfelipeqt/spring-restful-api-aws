bb658aa0e1da7f1dbbc9cb6017bc641b3f8b91c5
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-25 22:14:23 -0300

    Implementando o Serviço Responsável Pela Autenticação

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/AuthServices.java
@@ -0,0 +1,53 @@
+package br.com.erudio.services;
+
+import br.com.erudio.data.vo.v1.AccountCredentialsVO;
+import br.com.erudio.data.vo.v1.TokenVO;
+import br.com.erudio.repositories.UsersRepository;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signin(AccountCredentialsVO data){
+        try{
+            var username = data.getUserName();
+            var password = data.getPassword();
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password));
+            var user = usersRepository.findByUsername(username);
+
+            var tokenResponse = new TokenVO();
+            if(user != null){
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            }
+            else {
+                throw new UsernameNotFoundException("Username " + username + " not found!");
+            }
+            return ResponseEntity.ok(tokenResponse);
+
+        }
+        catch(Exception e){
+            throw new BadCredentialsException("Invalid username/password supplied");
+
+        }
+    }
+
+
+}
